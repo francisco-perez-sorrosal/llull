08e294ad189c51b4500b60cc98a31392e98c8469
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2022-02-01 20:40:31 -0800

    Add basic initial logging with loguru

--- src/rllull/__init__.py
+++ src/rllull/__init__.py
@@ -4,6 +4,12 @@ rllull package.
 Taxonomy Manager
 """
 
+import os
+from loguru import logger
 from typing import List
 
 __all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
+
+home = os.environ.get('HOME')
+logdir = os.environ.get('LULIO_LOGDIR', home)
+logger.add(f"{logdir}/lulio_log.log",rotation="500MB", encoding="utf-8", enqueue=True, retention="1 days")
--- src/rllull/taxonomy.py
+++ src/rllull/taxonomy.py
@@ -1,4 +1,5 @@
 import dataclasses
+from loguru import logger
 from typing import Dict, List, Optional, Union
 
 from .taxon import Taxon
@@ -14,21 +15,21 @@ class Taxonomy:
     def add(self, parent_id: str, node: Taxon):
         def add(current: Taxon, parent_id: str, node: Taxon, level: int):
             if parent_id == current.name:
-                # print(f"Including {node.name} in {root}")
+                logger.debug(f"Including {node.name} in {current}")
                 node.level = level
                 node.parent = current
                 current.children[node.name] = node
                 self.search_table[node.name] = node
                 return 1
             else:
-                count = 0
+                count = 0                
                 for k in current.children.keys():
-                    # print(f"key {k}")
+                    logger.debug(f"key {k}")
                     count += add(current.children[k], parent_id, node, level + 1)
                 return count
 
         if node.name in self.search_table:
-            print(f"Self node {node} already in the taxonomy. Skipping...")
+            logger.debug(f"Self node {node} already in the taxonomy. Skipping...")
             return
         # Root is empty then the node will become the root
         if parent_id is None:
@@ -37,18 +38,15 @@ class Taxonomy:
                 node.parent = None
                 self.root = node
                 self.search_table[node.name] = node
-                print(f"Root node setup to taxon {node}")
+                logger.info(f"Root nodee setup to taxon {node}")
                 self.nodes_count = 1
         else:
             assert self.root is not None
             self.nodes_count += add(self.root, parent_id, node, 1)
-            # print(self.nodes_count)
 
     def taxons_in_level(self, level: int) -> List[Taxon]:
         def get_level_from_children(level: int, current_taxon: Taxon = None) -> List[Taxon]:
             level_taxons: List[Taxon] = []
-            # if not current_taxon:
-            #     return level_taxons
 
             assert current_taxon is not None
             if len(current_taxon.children) > 0:
@@ -74,7 +72,7 @@ class Taxonomy:
         lineage = []
         current_taxon = self.search_table.get(taxon_id, None)
         if not current_taxon:
-            print("Sorry, taxon {taxon_id} not found")
+            logger.info("Sorry, taxon {taxon_id} not found")
         else:
             lineage.append(current_taxon)
             while current_taxon.parent:
